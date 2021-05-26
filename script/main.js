3455473b96e498ed3a79e26e8b7fc8df79a8d4b0
etpotato <mareev.pv@gmail.com>
2021-05-26 15:29:58 +0300

    Autoplay policy is OK

--- script/main.js
+++ script/main.js
@@ -3,15 +3,21 @@ const playButton = document.querySelector('.page__play');
 const audio = document.querySelector('.page__audio');
 const volumeButton = document.querySelector('.page__volume');
 
-const audioContext = new AudioContext();
-const track = audioContext.createMediaElementSource(audio);
-const gainKnob = audioContext.createGain();
+let audioContext = null;
+let track = null;
+let gainKnob = null;
 
 const onPlayAudio = (evt) => {
   evt.preventDefault();
-  if (audioContext.state === 'suspended') {
-    audioContext.resume();
-  }
+  const isMuted = volumeButton.getAttribute('aria-checked') === 'true';
+  audioContext = new AudioContext();
+  track = audioContext.createMediaElementSource(audio);
+  gainKnob = audioContext.createGain();
+
+  track.connect(gainKnob).connect(audioContext.destination);
+
+  if (isMuted) gainKnob.gain.value = 0;
+
   audio.play();
   playButton.setAttribute('aria-checked', 'true');
 };
@@ -25,15 +31,17 @@ const onPlayButtonClick = (evt) => {
 
 const onVolumeButtonClick = (evt) => {
   evt.preventDefault();
+
+  if (gainKnob) {
+    evt.target.matches('.page__volume--mute') 
+    ? gainKnob.gain.value = 1 
+    : gainKnob.gain.value = 0;
+  }
+  
   const isChecked = volumeButton.getAttribute('aria-checked') === 'true';
   volumeButton.setAttribute('aria-checked', !isChecked)
-  evt.target.matches('.page__volume--mute') 
-  ? gainKnob.gain.value = 1 
-  : gainKnob.gain.value = 0;
   volumeButton.classList.toggle('page__volume--mute');
 };
-
-track.connect(gainKnob).connect(audioContext.destination);
 
 playButton.addEventListener('click', onPlayAudio, {once: true});
 playButton.addEventListener('click', onPlayButtonClick);
