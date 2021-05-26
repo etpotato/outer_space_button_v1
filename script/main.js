39be81dd13d3908317c53d530f0d3f8a3832c440
etpotato <mareev.pv@gmail.com>
2021-05-26 14:48:01 +0300

    Rewrite script for webaudio api

--- script/main.js
+++ script/main.js
@@ -3,16 +3,38 @@ const playButton = document.querySelector('.page__play');
 const audio = document.querySelector('.page__audio');
 const volumeButton = document.querySelector('.page__volume');
 
-playButton.addEventListener('click', (evt) => {
+const audioContext = new AudioContext();
+const track = audioContext.createMediaElementSource(audio);
+const gainKnob = audioContext.createGain();
+
+const onPlayAudio = (evt) => {
+  evt.preventDefault();
+  if (audioContext.state === 'suspended') {
+    audioContext.resume();
+  }
+  audio.play();
+  playButton.setAttribute('aria-checked', 'true');
+};
+
+const onPlayButtonClick = (evt) => {
   evt.preventDefault();
   const random = Math.random() * 360;
   root.style.setProperty('--hue', random); 
   root.style.setProperty('--angle', `${random}turn`);
-  audio.play();
-});
+};
 
-volumeButton.addEventListener('click', (evt) => {
+const onVolumeButtonClick = (evt) => {
   evt.preventDefault();
-  evt.target.matches('.page__volume--mute') ? audio.volume = 1 : audio.volume = 0;
+  const isChecked = volumeButton.getAttribute('aria-checked') === 'true';
+  volumeButton.setAttribute('aria-checked', !isChecked)
+  evt.target.matches('.page__volume--mute') 
+  ? gainKnob.gain.value = 1 
+  : gainKnob.gain.value = 0;
   volumeButton.classList.toggle('page__volume--mute');
-});
+};
+
+track.connect(gainKnob).connect(audioContext.destination);
+
+playButton.addEventListener('click', onPlayAudio, {once: true});
+playButton.addEventListener('click', onPlayButtonClick);
+volumeButton.addEventListener('click', onVolumeButtonClick);
